2f1ad7ebefd603b9d429c4c04757c8ef3feac688
Akash Mahadik <akashpm1996@gmail.com>
2024-10-29 06:29:12 +0530

    Changes: - Implement swiper autoplay and pagination controls behavior to match with apple - Refactor few class names

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,68 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, reactive, useTemplateRef } from 'vue';
 import Swiper from 'swiper';
-import { Pagination, Keyboard } from 'swiper/modules';
+import { Pagination, Keyboard, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-onMounted(() => {
-  const slider = new Swiper('.slider', {
-    modules: [Pagination, Keyboard],
-    slidesPerView: 1,
-    spaceBetween: 20,
-    grabCursor: true,
-    speed: 500,
-    keyboard: {
-      enabled: true
-    },
-    pagination: {
-      el: '.slider__pagination',
-      clickable: true,
+
+const highlightsSwiperEl = useTemplateRef('highlights_swiper');
+
+const highlightsSwiperParams = {
+  modules: [Pagination, Keyboard, Autoplay],
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  speed: 1500,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+  },
+  keyboard: {
+    enabled: true
+  },
+  pagination: {
+    el: '.swiper__pagination',
+    clickable: true,
+  },
+}
+
+const highlightsSwiperProps = reactive({
+  isAutoplayRunning: true,
+  isAutoplayEnded: false
+});
+
+const toggleSliderAutoplay = () => {
+  if (!highlightsSwiperEl.value.swiper.autoplay.running) {
+    if (highlightsSwiperEl.value.swiper.isEnd) {
+      highlightsSwiperEl.value.swiper.slideTo(0, 1500, false);
     }
-  });
+    highlightsSwiperEl.value.swiper.autoplay.start();
+    highlightsSwiperProps.isAutoplayRunning = true;
+  } else {
+    highlightsSwiperEl.value.swiper.autoplay.stop();
+    highlightsSwiperProps.isAutoplayRunning = false;
+  }
+}
+
+const initHighlightsSwiper = () => {
+  const highlightsSwiper = new Swiper(highlightsSwiperEl.value, highlightsSwiperParams);
+
+  highlightsSwiper.on('reachEnd', () => {
+    setTimeout(() => {
+      highlightsSwiper.autoplay.stop();
+      highlightsSwiperProps.isAutoplayEnded = true;
+    }, 3999);
+  })
+
+  highlightsSwiper.on('slideChange', () => {
+    highlightsSwiperProps.isAutoplayRunning = highlightsSwiper.autoplay.running;
+    highlightsSwiperProps.isAutoplayEnded = false;
+  })
+}
+
+onMounted(() => {
+  initHighlightsSwiper();
 })
 </script>
 
@@ -101,8 +144,8 @@ onMounted(() => {
       <div class="w-[87.5%] max-w-[1680px] mx-auto">
         <h2 class="text-[56px] leading-none tracking-tighter font-semibold pb-20">Get the highlights.</h2>
       </div>
-      <div
-        class="slider px-[max(6.25vw,(100vw-1680px)/2,env(safe-area-inset-left),env(safe-area-inset-right))] snap-x snap-mandatory">
+      <div ref="highlights_swiper"
+        class="highlights_swiper px-[max(6.25vw,(100vw-1680px)/2,env(safe-area-inset-left),env(safe-area-inset-right))] snap-x snap-mandatory">
         <div class="swiper-wrapper">
           <div
             class="swiper-slide relative overflow-hidden w-[min(max(87.5vw,280px),1680px)] min-h-[740px] bg-[rgb(232,232,237)] rounded-[28px]">
@@ -161,13 +204,32 @@ onMounted(() => {
             </div>
           </div>
         </div>
-        <div class="slider__controls sticky bottom-8 my-8 text-center h-14 z-10">
+        <div class="swiper__controls sticky bottom-8 my-8 text-center h-14 z-10">
           <div class="inline-flex h-full">
+            <button
+              class="relative h-full aspect-square me-4 rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg"
+              @click="toggleSliderAutoplay">
+              <svg v-show="!highlightsSwiperProps.isAutoplayRunning && !highlightsSwiperProps.isAutoplayEnded"
+                id="play-icon" class="absolute inset-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56">
+                <path
+                  d="m23.7555 36.6237c.4478 0 .8598-.1343 1.4241-.4568l10.9178-6.3322c.8598-.5016 1.3614-1.021 1.3614-1.8361 0-.8061-.5016-1.3255-1.3614-1.8271l-10.9178-6.3322c-.5643-.3314-.9762-.4657-1.4241-.4657-.9315 0-1.7555.7165-1.7555 1.9435v13.3629c0 1.227.824 1.9435 1.7555 1.9435z">
+                </path>
+              </svg>
+              <svg v-show="highlightsSwiperProps.isAutoplayRunning && !highlightsSwiperProps.isAutoplayEnded"
+                id="pause-icon" class="absolute inset-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56">
+                <path
+                  d="m21.7334 36.67h2.5342c1.1483 0 1.7324-.5796 1.7324-1.7193v-13.9015c0-1.12-.5841-1.6898-1.7324-1.7193h-2.5342c-1.1483 0-1.7324.5698-1.7324 1.7193v13.9015c-.0297 1.1396.5544 1.7193 1.7324 1.7193zm9.9992 0h2.5347c1.1485 0 1.7327-.5796 1.7327-1.7193v-13.9015c0-1.12-.5842-1.7193-1.7327-1.7193h-2.5347c-1.1485 0-1.7327.5698-1.7327 1.7193v13.9015c0 1.1396.5545 1.7193 1.7327 1.7193z">
+                </path>
+              </svg>
+              <svg v-show="highlightsSwiperProps.isAutoplayEnded" id="replay-icon" class="absolute inset-0"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56">
+                <path
+                  d="m36.2448 26.6447c-1.1073 0-2.0052.8978-2.0052 2.0052 0 3.4405-2.7992 6.2397-6.2397 6.2397s-6.2397-2.7992-6.2397-6.2397 2.7992-6.2397 6.2397-6.2397c.0283 0 .0546-.0072.0825-.0083l-1.2839 1.2841c-.7833.7828-.7833 2.0526 0 2.8354.3911.3916.9047.5874 1.4177.5874s1.0266-.1958 1.4177-.5874l4.4406-4.4406c.7833-.7828.7833-2.0526 0-2.8354l-4.657-4.657c-.7823-.7833-2.0531-.7833-2.8354 0-.7833.7828-.7833 2.0526 0 2.8354l.9973.9974c-5.4561.223-9.8295 4.7189-9.8295 10.2287 0 5.6517 4.5983 10.25 10.25 10.25s10.25-4.5983 10.25-10.25c0-1.1073-.8978-2.0052-2.0052-2.0052z">
+                </path>
+              </svg>
+            </button>
             <div
-              class="relative h-full aspect-square me-4 rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg">
-            </div>
-            <div
-              class="slider__pagination h-full rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg flex items-center px-4">
+              class="swiper__pagination h-full rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg flex items-center px-4">
             </div>
           </div>
         </div>
@@ -176,7 +238,7 @@ onMounted(() => {
   </main>
 </template>
 <style>
-.slider__pagination {
+.swiper__pagination {
   & .swiper-pagination-bullet {
     display: inline-block;
     background: rgba(29, 29, 31, 0.6);
