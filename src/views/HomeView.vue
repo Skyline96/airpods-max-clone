2cb7f535dabf0c13cd395611befd2607af39b9af
Akash Mahadik <akashpm1996@gmail.com>
2024-11-01 12:20:11 +0530

    Initial layout for airpods colors showcase slider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { onMounted, reactive, useTemplateRef } from 'vue';
 import Swiper from 'swiper';
-import { Pagination, Keyboard, Autoplay } from 'swiper/modules';
+import { Pagination, Keyboard, Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 
 const highlightsSwiperEl = useTemplateRef('highlights_swiper');
+const colorsSwiperEl = useTemplateRef('colors_swiper');
 
 const highlightsSwiperParams = {
   modules: [Pagination, Keyboard, Autoplay],
@@ -22,9 +23,24 @@ const highlightsSwiperParams = {
     enabled: true
   },
   pagination: {
-    el: '.swiper__pagination',
+    el: '.highlights_swiper__pagination',
     clickable: true,
   },
+}
+
+const colorsSwiperParams = {
+  modules: [Keyboard, Navigation],
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  speed: 1500,
+  keyboard: {
+    enabled: true
+  },
+  navigation: {
+    prevEl: '.prev',
+    nextEl: '.next'
+  }
 }
 
 const highlightsSwiperProps = reactive({
@@ -64,11 +80,15 @@ const initHighlightsSwiper = () => {
       }, 3999);
     }
   });
+}
 
+const initColorsSwiper = () => {
+  const colorsSwiper = new Swiper(colorsSwiperEl.value, colorsSwiperParams);
 }
 
 onMounted(() => {
   initHighlightsSwiper();
+  initColorsSwiper();
 })
 </script>
 
@@ -99,7 +119,7 @@ onMounted(() => {
         </div>
       </div>
     </section>
-    <section class="highlights relative pt-40 pb-52 overflow-x-clip">
+    <section class="highlights relative py-40 overflow-x-clip">
       <div class="w-[87.5%] max-w-[1680px] mx-auto">
         <h2 class="text-[56px] leading-none tracking-tighter font-semibold pb-20">Get the highlights.</h2>
       </div>
@@ -163,7 +183,7 @@ onMounted(() => {
             </div>
           </div>
         </div>
-        <div class="swiper__controls sticky bottom-8 my-8 text-center h-14 z-10">
+        <div class="swiper__controls sticky bottom-8 mt-8 text-center h-14 z-10">
           <div class="inline-flex h-full">
             <button
               class="relative h-full aspect-square me-4 rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg"
@@ -188,8 +208,68 @@ onMounted(() => {
               </svg>
             </button>
             <div
-              class="swiper__pagination h-full rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg flex items-center px-4">
+              class="highlights_swiper__pagination h-full rounded-[100vmax] bg-[rgba(232,232,237,0.7)] backdrop-blur-lg flex items-center px-4">
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="gallery relative pb-40 overflow-x-clip">
+      <div class="w-[87.5%] max-w-[1680px] mx-auto">
+        <h2 class="text-[56px] leading-none tracking-tighter font-semibold pb-20">Take a closer look.</h2>
+      </div>
+      <div ref="colors_swiper"
+        class="colors_swiper px-[max(6.25vw,(100vw-1680px)/2,env(safe-area-inset-left),env(safe-area-inset-right))]">
+        <div class="swiper-wrapper">
+          <div
+            class="swiper-slide relative overflow-hidden w-[min(max(87.5vw,280px),1680px)] min-h-[740px] bg-[rgb(232,232,237)] rounded-[28px]">
+            <div class="absolute inset-0">
+              <img src="/images/colors/bento_1_airpod_max_midnight_xlarge.jpg" alt="" class="h-full object-cover">
+            </div>
+          </div>
+          <div class="swiper-slide w-[min(max(87.5vw,280px),1680px)] min-h-[740px]">
+            <div class="absolute inset-0 grid grid-cols-2 gap-[20px]">
+              <div class="relative h-full rounded-[28px] bg-[rgb(232,232,237)] overflow-clip">
+                <img src="/images/colors/bento_2_airpod_max_midnight_xlarge.jpg" alt="" class="min-h-full object-cover">
+              </div>
+              <div class="relative h-full rounded-[28px] bg-[rgb(232,232,237)] overflow-clip">
+                <img src="/images/colors/bento_3_airpod_max_midnight_xlarge.jpg" alt="" class="min-h-full object-cover">
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide w-[min(max(87.5vw,280px),1680px)] min-h-[740px]">
+            <div class="absolute inset-0 grid grid-cols-2 gap-[20px]">
+              <div class="relative h-full rounded-[28px] bg-[rgb(232,232,237)] overflow-clip">
+                <img src="/images/colors/bento_4_airpod_max_midnight_xlarge.jpg" alt="" class="min-h-full object-cover">
+              </div>
+              <div class="relative h-full rounded-[28px] bg-[rgb(232,232,237)] overflow-clip">
+                <img src="/images/colors/bento_5_airpod_max_midnight_xlarge.jpg" alt="" class="min-h-full object-cover">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="colors_swiper__navigation flex justify-end gap-4 pt-12">
+          <div class="w-9 h-9">
+            <button
+              class="prev w-full h-full rounded-[100vmax] bg-[rgba(210,210,215,0.64)] text-black/50 disabled:opacity-50">
+              <svg class="icon-control icon-control-chevronleft w-full h-full fill-current"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+                <path
+                  d="m20 25c-.3838 0-.7676-.1465-1.0605-.4395l-5.5-5.5c-.5859-.5854-.5859-1.5356 0-2.1211l5.5-5.5c.5859-.5859 1.5352-.5859 2.1211 0 .5859.5854.5859 1.5356 0 2.1211l-4.4395 4.4395 4.4395 4.4395c.5859.5854.5859 1.5356 0 2.1211-.293.293-.6768.4395-1.0605.4395z">
+                </path>
+              </svg>
+            </button>
+          </div>
+          <div class="w-9 h-9">
+            <button
+              class="next w-full h-full rounded-[100vmax] bg-[rgba(210,210,215,0.64)] text-black/50 disabled:opacity-50">
+              <svg class="icon-control icon-control-chevronright fill-current" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 36 36">
+                <path
+                  d="m22.5597 16.9375-5.5076-5.5c-.5854-.5854-1.5323-.5825-2.1157.0039-.5835.5869-.5815 1.5366.0039 2.1211l4.4438 4.4375-4.4438 4.4375c-.5854.5845-.5874 1.5342-.0039 2.1211.2922.2944.676.4414 1.0598.4414.3818 0 .7637-.1455 1.0559-.4375l5.5076-5.5c.2815-.2812.4403-.6636.4403-1.0625s-.1588-.7812-.4403-1.0625z">
+                </path>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
@@ -197,7 +277,7 @@ onMounted(() => {
   </main>
 </template>
 <style>
-.swiper__pagination {
+.highlights_swiper__pagination {
   & .swiper-pagination-bullet {
     display: inline-block;
     background: rgba(29, 29, 31, 0.6);
