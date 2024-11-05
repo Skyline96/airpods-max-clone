ff1be0e21044a61f4329ded222c8e5b1c2ca0e3a
Akash Mahadik <akashpm1996@gmail.com>
2024-11-05 07:02:33 +0530

    Add 'hifi sound' section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -289,18 +289,81 @@ onMounted(() => {
             </button>
           </div>
         </div>
-        <div class="sticky bottom-8 inset-x-0 max-w-max mx-auto my-8 text-center h-14 z-10">
-          <div class="h-full bg-[rgba(232,232,237,0.7)] backdrop-blur-lg rounded-[100vmax] overflow-hidden">
-            <ul class="list-none color_swatches h-full inline-flex items-center mx-2">
-              <li v-for="color in availableColorVariants" class="w-7 h-7 mx-2">
-                <input type="radio" name="color_swatch" v-model="colorVariant" :value="color.colorName"
-                  class="appearance-none relative h-full w-full rounded-[100vmax] focus:outline focus:outline-2 focus:outline-offset-4 focus:outline-black/60 after:content-[''] after:absolute after:-inset-[1px] after:rounded-[inherit] after:bg-gradient-to-b after:from-black/60 after:to-55% after:-z-10"
-                  :class="isColorVariantActive(color.colorName) ? 'outline outline-2 outline-offset-4 outline-black/60' : ''"
-                  :style="`background:linear-gradient(0deg, ${color.colorShadeLight} 50%, ${color.colorShadeDark} 0);`" />
-              </li>
-            </ul>
+      </div>
+      <div class="sticky bottom-8 inset-x-0 max-w-max mx-auto mt-8 text-center h-14 z-10">
+        <div class="h-full bg-[rgba(232,232,237,0.7)] backdrop-blur-lg rounded-[100vmax] overflow-hidden">
+          <ul class="list-none color_swatches h-full inline-flex items-center mx-2">
+            <li v-for="color in availableColorVariants" class="w-7 h-7 mx-2">
+              <input type="radio" name="color_swatch" v-model="colorVariant" :value="color.colorName"
+                class="appearance-none relative h-full w-full rounded-[100vmax] focus:outline focus:outline-2 focus:outline-offset-4 focus:outline-black/60 after:content-[''] after:absolute after:-inset-[1px] after:rounded-[inherit] after:bg-gradient-to-b after:from-black/60 after:to-55% after:-z-10"
+                :class="isColorVariantActive(color.colorName) ? 'outline outline-2 outline-offset-4 outline-black/60' : ''"
+                :style="`background:linear-gradient(0deg, ${color.colorShadeLight} 50%, ${color.colorShadeDark} 0);`" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section class="hifi_sound_details relative">
+      <div class="details-bg absolute inset-0 -z-10">
+        <img src="/images/audio_airpod_max_large.jpg" alt="" class="min-h-full object-cover">
+      </div>
+      <div
+        class="flex flex-col justify-between items-center w-[87.5%] max-w-[1680px] min-h-[2280px] mx-auto pt-40 pb-[88px] text-white">
+        <div class="mb-10 text-center">
+          <h2 class="text-[110px] leading-[1.15] tracking-[-3px] font-semibold">All the vivid details.</h2>
+          <p class="text-[28px] leading-[1.15] text-balance font-semibold mt-10">AirPods Max deliver stunningly
+            detailed, high-fidelity audio for an unparalleled listening experience. Each
+            part of the custom-built driver works to produce sound with ultra-low distortion across the audible range —
+            so you’ll hear every note with a new sense of clarity.</p>
+        </div>
+        <div class="grid grid-cols-2 gap-24">
+          <div class="">
+            <div class="h-[60px] mb-[18px]">
+              <svg class="hifi_sound_detail_icon_spatial_audio max-h-full fill-current"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.44 26.77">
+                <path class="cls-1"
+                  d="M17.2,1.38a.37.37,0,0,0-.14.5h0A1,1,0,0,1,17,3.12,3,3,0,0,1,15.68,4a10.21,10.21,0,0,1-3.46.56A10.11,10.11,0,0,1,8.77,4a3,3,0,0,1-1.32-.84,1,1,0,0,1-.08-1.23h0a.36.36,0,0,0-.06-.44.37.37,0,0,0-.52,0A1.87,1.87,0,0,0,6.25,2.5a2.11,2.11,0,0,0,.28,1.24A4.09,4.09,0,0,0,8.22,5.17a9.23,9.23,0,0,0,4,.85,9.2,9.2,0,0,0,4-.85,4.17,4.17,0,0,0,1.69-1.43,2,2,0,0,0,.27-1.24,1.8,1.8,0,0,0-.54-1.06.35.35,0,0,0-.44-.06ZM15.2,0a.31.31,0,0,0-.15.41v0a.48.48,0,0,1-.08.62,2.15,2.15,0,0,1-.75.43,6.35,6.35,0,0,1-2,.3,6.09,6.09,0,0,1-2-.31,1.77,1.77,0,0,1-.74-.43A.47.47,0,0,1,9.38.46v0A.31.31,0,0,0,9.34.1a.32.32,0,0,0-.44,0,1.16,1.16,0,0,0-.37.69,1.37,1.37,0,0,0,.17.82,2.52,2.52,0,0,0,1.06.93,5.5,5.5,0,0,0,2.46.54,5.5,5.5,0,0,0,2.46-.54,2.46,2.46,0,0,0,1.06-.93,1.45,1.45,0,0,0,.17-.82,1.16,1.16,0,0,0-.37-.69.3.3,0,0,0-.34,0Zm7.86,17.22a.35.35,0,0,0-.5-.13h0A1,1,0,0,1,21.32,17a3,3,0,0,1-.84-1.31,10.21,10.21,0,0,1-.56-3.46,10.19,10.19,0,0,1,.56-3.45,3,3,0,0,1,.84-1.32,1,1,0,0,1,1.23-.07h0A.36.36,0,0,0,23,7.37a.36.36,0,0,0,0-.52,1.8,1.8,0,0,0-1.06-.54,2,2,0,0,0-1.24.27,4.13,4.13,0,0,0-1.43,1.7,9.16,9.16,0,0,0-.85,4,9.2,9.2,0,0,0,.85,4A4.17,4.17,0,0,0,20.7,18a2.13,2.13,0,0,0,1.24.28A1.87,1.87,0,0,0,23,17.69.35.35,0,0,0,23.06,17.25Zm1.35-2a.3.3,0,0,0-.4-.15l0,0a.47.47,0,0,1-.61-.09,1.75,1.75,0,0,1-.43-.75,6,6,0,0,1-.31-2,6.09,6.09,0,0,1,.31-2,1.77,1.77,0,0,1,.43-.74A.47.47,0,0,1,24,9.43h0a.31.31,0,0,0,.33,0,.31.31,0,0,0,0-.44,1.16,1.16,0,0,0-.69-.37,1.45,1.45,0,0,0-.82.17,2.52,2.52,0,0,0-.93,1.06,5.5,5.5,0,0,0-.54,2.46,5.47,5.47,0,0,0,.54,2.46,2.54,2.54,0,0,0,.93,1.07,1.37,1.37,0,0,0,.82.16,1.16,1.16,0,0,0,.69-.37.3.3,0,0,0,.05-.34ZM1.88,17.12a.35.35,0,0,0-.44.05.36.36,0,0,0,0,.52,1.87,1.87,0,0,0,1.06.55A2.11,2.11,0,0,0,3.74,18a4.17,4.17,0,0,0,1.43-1.69,9.2,9.2,0,0,0,.85-4,9.16,9.16,0,0,0-.85-4,4.13,4.13,0,0,0-1.43-1.7A2,2,0,0,0,2.5,6.31a1.8,1.8,0,0,0-1.06.54.37.37,0,0,0,.44.58h0a1,1,0,0,1,1.23.07A3,3,0,0,1,4,8.82a10.19,10.19,0,0,1,.56,3.45A10.21,10.21,0,0,1,4,15.73,3,3,0,0,1,3.12,17a1,1,0,0,1-1.23.08Zm-1.45-2a.29.29,0,0,0-.33.06.3.3,0,0,0,0,.43A1.16,1.16,0,0,0,.77,16a1.37,1.37,0,0,0,.82-.16,2.54,2.54,0,0,0,.93-1.07,5.47,5.47,0,0,0,.54-2.46,5.5,5.5,0,0,0-.54-2.46,2.52,2.52,0,0,0-.93-1.06,1.45,1.45,0,0,0-.82-.17A1.16,1.16,0,0,0,.08,9a.32.32,0,0,0,0,.34.3.3,0,0,0,.4.15h0a.47.47,0,0,1,.61.09,1.89,1.89,0,0,1,.44.74,6.35,6.35,0,0,1,.3,2,6.27,6.27,0,0,1-.3,2,2,2,0,0,1-.43.75.47.47,0,0,1-.62.08h0Zm15.73-2.83a4.1,4.1,0,0,1-3.94,4.25,4.1,4.1,0,0,1-3.94-4.25A4.1,4.1,0,0,1,12.22,8,4.1,4.1,0,0,1,16.16,12.27Zm-3.94,6C7.13,18.28,4,21.78,4,24.92c0,1.23.78,1.85,2.31,1.85H18.16c1.53,0,2.31-.62,2.31-1.85,0-3.14-3.16-6.64-8.25-6.64Z">
+                </path>
+              </svg>
+            </div>
+            <p class="text-[21px] font-semibold leading-tight tracking-tight text-[#808488]"><span
+                class="text-white">Personalized
+                Spatial Audio</span> with dynamic head tracking gives you a theater‑like experience with
+              sound that surrounds you for movies, TV shows, and games. Using built-in gyroscopes and accelerometers,
+              AirPods Max and your iPhone, iPad, Mac, or Apple TV track the subtle motion of your head, anchoring sounds
+              to your device.</p>
+          </div>
+          <div class="">
+            <div class="h-[60px] mb-[18px]">
+              <svg class="hifi-sound-detail-icon-adaptive-eq max-h-full fill-current" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 21.1 25.1">
+                <path class="cls-1"
+                  d="M20.55,16.59A.55.55,0,0,1,20,16V9a.55.55,0,0,1,1.1,0v7A.55.55,0,0,1,20.55,16.59ZM17.1,21.67V3.42a.55.55,0,0,0-1.1,0V21.67a.55.55,0,0,0,1.1,0Zm-3.89-3.12v-12A.55.55,0,0,0,12.66,6a.55.55,0,0,0-.55.55v12a.55.55,0,0,0,.55.55A.55.55,0,0,0,13.21,18.55Zm-4.11,6V.55A.55.55,0,0,0,8.55,0,.55.55,0,0,0,8,.55v24a.55.55,0,0,0,.55.55A.55.55,0,0,0,9.1,24.55Zm-4-5v-14a.55.55,0,0,0-1.1,0v14a.55.55,0,1,0,1.1,0Zm-4-4.75V10.3a.55.55,0,0,0-.55-.55A.55.55,0,0,0,0,10.3v4.5a.55.55,0,0,0,.55.55A.55.55,0,0,0,1.1,14.8Z">
+                </path>
+              </svg>
+            </div>
+            <p class="text-[21px] font-semibold leading-tight tracking-tight text-[#808488]"><span
+                class="text-white">Adaptive
+                EQ</span> tailors sound to the bespoke fit and seal created by the ear cushions. Inward-facing
+              microphones measure what you’re hearing, then adjust the frequencies of your music to deliver a rich,
+              consistent experience that faithfully reproduces every note.</p>
           </div>
         </div>
+      </div>
+      <div class="sticky bottom-8 inset-x-0 max-w-max mx-auto mt-8 text-center h-14 z-10">
+        <button
+          class="h-full bg-[rgba(66,66,69,0.7)] text-white backdrop-blur rounded-[100vmax] px-1 overflow-hidden outline-blue-500 focus:outline focus:outline-2">
+          <span class="h-full inline-flex items-center gap-4 ms-5 me-2">
+            <span class=" text-[17px] font-semibold">Learn more about high-fidelity audio</span>
+            <span class="relative h-9 w-9 rounded-[100vmax] bg-blue-500">
+              <svg class="icon-control-plus fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+                <path
+                  d="m24 16.5h-4.5v-4.5c0-.8286-.6719-1.5-1.5-1.5s-1.5.6714-1.5 1.5v4.5h-4.5c-.8281 0-1.5.6714-1.5 1.5s.6719 1.5 1.5 1.5h4.5v4.5c0 .8286.6719 1.5 1.5 1.5s1.5-.6714 1.5-1.5v-4.5h4.5c.8281 0 1.5-.6714 1.5-1.5s-.6719-1.5-1.5-1.5z">
+                </path>
+              </svg>
+            </span>
+          </span>
+        </button>
       </div>
     </section>
   </main>
