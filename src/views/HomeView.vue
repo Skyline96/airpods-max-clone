41ed5b93a2166cec91285452854ebf67c148698d
Akash Mahadik <akashpm1996@gmail.com>
2024-11-09 17:49:57 +0530

    Add 'transparency mode' layout to 'product stories' section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -469,7 +469,7 @@ onMounted(() => {
         </Teleport>
       </div>
     </section>
-    <section class="product_stories relative bg-[#f5f5f7]">
+    <section class="product_stories relative bg-[#f5f5f7] overflow-hidden">
       <div class="anc w-[87.5%] max-w-[1680px] pt-40 mx-auto">
         <div class="section_header text-center mb-24">
           <h2 class="text-[110px] leading-[1.15] tracking-[-3px] font-semibold">Unheard-of sound.</h2>
@@ -496,6 +496,31 @@ onMounted(() => {
               you hear — and don’t hear — you can immerse yourself in music and podcasts, or simply stay focused, like
               never before.
             </p>
+          </div>
+        </div>
+        <div class="transparency_mode_contianer flex">
+          <div class="relative max-w-[25%] ms-[8.33%] mt-60 mb-20">
+            <div
+              class="horizontal_pin box-border flex items-center absolute z-10 -top-9 left-0 -right-[540px] before:content-[''] before:z-10 before:absolute before:w-2 before:h-2 before:right-0 before:bg-[rgb(134,134,139)] before:m-[-3px] before:rounded after:content-[''] after:w-full after:h-[1.5px] after:bg-[rgb(134,134,139)]">
+            </div>
+            <div class="h-[55px] mb-[18px]">
+              <svg class="anc-detail-icon-transparency max-h-full fill-current" viewBox="0 0 31 39.61"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="m26.33 23.73c.07-.07.13-.15.19-.23.05-.08.1-.17.14-.26.03-.09.06-.18.08-.28s.03-.2.03-.29c0-.4-.16-.78-.44-1.06-.56-.56-1.56-.56-2.12 0-.28.28-.44.66-.44 1.06 0 .09 0 .19.03.29.02.1.05.19.09.28.03.09.08.18.14.26.05.08.11.16.18.23.28.28.67.44 1.06.44s.78-.16 1.06-.44zm1.39-6.19c.76 0 1.41-.57 1.49-1.34.09-.83-.51-1.56-1.34-1.65-.82-.08-1.56.52-1.64 1.34-.09.83.51 1.56 1.34 1.65zm-24.45 0s.1-.01.16-.01c.82-.09 1.42-.83 1.33-1.65-.08-.82-.82-1.42-1.65-1.34-.82.09-1.42.83-1.33 1.66.08.76.73 1.34 1.49 1.34zm23.4-6.98c.2 0 .41-.04.6-.13.76-.33 1.11-1.22.77-1.97-.33-.76-1.21-1.11-1.97-.78-.76.34-1.11 1.22-.77 1.98.24.56.79.9 1.37.9zm-22.37-.01c.58 0 1.13-.33 1.38-.9.33-.76 0-1.64-.77-1.97-.76-.34-1.64 0-1.98.77-.33.75 0 1.64.77 1.97.2.09.4.13.6.13zm17.9-5.44c.48 0 .95-.23 1.24-.65.47-.68.29-1.62-.39-2.09s-1.61-.29-2.08.39c-.48.68-.3 1.62.38 2.09.26.17.56.26.85.26zm-13.41 0c.29 0 .59-.09.85-.27.68-.47.85-1.4.38-2.1-.47-.68-1.4-.85-2.09-.38-.68.47-.85 1.41-.38 2.09.29.42.76.65 1.24.65zm6.71-2.1c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.49.67 1.51 1.5 1.51zm-9.42 21.13c.1-.02.19-.05.28-.09.09-.03.18-.08.26-.14.08-.05.16-.11.23-.18s.13-.15.18-.23c.06-.08.1-.17.14-.26s.07-.18.09-.28.03-.19.03-.29c0-.4-.16-.78-.44-1.06-.56-.56-1.57-.56-2.12 0-.28.28-.44.66-.44 1.06 0 .1 0 .19.03.29 0 .1.04.19.08.28s.09.18.14.26.12.16.19.23c.28.28.66.44 1.06.44.1 0 .19-.01.29-.03zm20.59 15.47h-22.34c-2.87 0-4.33-1.08-4.33-3.21 0-5.36 6.23-11.1 15.5-11.1s15.5 5.74 15.5 11.1c0 2.13-1.46 3.21-4.33 3.21zm-11.17-17.44c-4.2 0-7.49-3.56-7.49-8.09s3.36-7.9 7.49-7.9 7.49 3.54 7.49 7.9-3.29 8.09-7.49 8.09z">
+                </path>
+              </svg>
+            </div>
+            <p class="text-[21px] font-semibold leading-tight tracking-tight text-[#6c6c6c]"><span
+                class="text-[#1d1d1f]">Transparency mode.</span> Press the noise control button to switch to
+              Transparency mode, which lets outside sound in so you can interact naturally with your surroundings.
+            </p>
+          </div>
+          <div class="relative w-0">
+            <div class="relative left-[138px]">
+              <img src="/images/anc/anc_airpod_max_close_up_large.png" alt=""
+                class="block min-w-max w-[1414px] h-[884px]">
+            </div>
           </div>
         </div>
       </div>
